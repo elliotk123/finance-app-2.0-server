b8439627a73dec4356c58791ca4adf19a04c21d5
elliotk123 <32294584+elliotk123@users.noreply.github.com>
2020-01-18 18:00:41 +0000

    mopre frequency endpoitns

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const namedErrors = require('./utils/namedError.json');
 const app = express();
 const port = 3000;
 const frequencyRouter = require('./router/frequencyRouter');
@@ -6,4 +7,16 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json())
 app.use('/frequencies', frequencyRouter);
+app.use(function (err, req, res, next) {
+    console.log(err);
+    var errorCode = 500;
+    switch (err.message) {
+        case namedErrors.NotFound:
+            errorCode = 404;
+            break;
+        default:
+            errorCode = 500;
+    }
+    res.status(errorCode).send(err.message)
+})
 app.listen(port, () => console.log(`Listening on port ${port}!`));
--- controllers/frequencies/addFrequency.js
+++ controllers/frequencies/addFrequency.js
@@ -1,4 +1,12 @@
+var frequencyLogic = require('../../logic/frequencyLogic');
+
 exports.controller = (req, res) => {
-    console.log("Get Frequency By Id Request");
-    res.send(req.body);
+    return new Promise((resolve, reject) => {
+        console.log("add frequency request");
+        return frequencyLogic.addFrequency(req.body).then(result => {
+            res.send(result)
+        }).catch( err => {
+            next(err);
+        });
+    })   
 }
